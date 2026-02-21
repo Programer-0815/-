a31e1c159653fd0c54e6b6fc992b08df963ac293
Programer-0815 <xiaoq345@gmail.com>
2026-02-21 17:29:43 +0800

    Create sw.js

---
+++ sw.js
@@ -0,0 +1,11 @@
+self.addEventListener('install', (e) => {
+  self.skipWaiting();
+});
+
+self.addEventListener('push', (event) => {
+  const data = event.data.json();
+  self.registration.showNotification(data.title, {
+    body: data.body,
+    icon: 'icon.png'
+  });
+});
